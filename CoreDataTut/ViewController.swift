717df2a06e75eb91184101d5c0e0e19e9648db98
= <tomclifford@btinternet.com>
2021-03-01 12:57:18 +0000

    Initial Commit

---
+++ CoreDataTut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataTut
+//
+//  Created by Tom Clifford on 01/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
